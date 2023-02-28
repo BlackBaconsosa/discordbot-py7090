8dbbbd9d8b389a17ad278ac24efd50c4dcba6c7d
BlackBaconsosa <126476733+BlackBaconsosa@users.noreply.github.com>
2023-02-28 20:54:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,8 +22,10 @@ async def on_message(message):
     if message.content == f'{PREFIX}call':
         await message.channel.send("callback!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(f'{PREFIX}게임'):
+        await message.channel.send('아직 명령어가 구현되지 않았습니다. 죄송합니다 :/')
+    if message.content.startswith(f'{PREFIX}랜덤뽑기'):
+        await message.channel.send(str(random.randrange(1,100)))
 
 
 try:
